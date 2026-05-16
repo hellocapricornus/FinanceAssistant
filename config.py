b89bcc8658abfeb6d73ee87ed478b2c933fd2c7c
hellocapricornus <hellocapricornus@gmail.com>
2026-05-16 21:12:36 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,3 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("FINANCE_BOT_TOKEN")
 OWNER_ID = int(os.getenv("FINANCE_OWNER", "0"))
-
-DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
-SILICONFLOW_API_KEY = os.getenv("SILICONFLOW_API_KEY")
-DASHSCOPE_API_KEY = os.getenv("DASHSCOPE_API_KEY")
-ZHIPU_API_KEY = os.getenv("ZHIPU_API_KEY")
